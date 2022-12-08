35baa2dd11401420b1281b06b4ca1900ce6fda49
1056878 <120036688+1056878@users.noreply.github.com>
2022-12-08 09:30:04 +0000

    Update plotting.R

--- functions/plotting.R
+++ functions/plotting.R
@@ -3,7 +3,7 @@
 plot_body_violin <- function(data) {
   data %>% 
     ggplot(aes(x = species, y = body_mass_g, colour = species, fill = species)) +
-    geom_violin(alpha = 0.3) +
+    geom_violin(alpha = 0.3, show.legend = FALSE) +
     theme_light() +
     # Renames the labels of the figure for clarity
     labs(x = "Species", y = "Body mass (g)", colour = "Species", fill = "Species") + 
