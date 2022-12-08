4cf98cbb57cd2d9bc41105b2d68aaa106e5abbb4
1056878 <120036688+1056878@users.noreply.github.com>
2022-12-08 09:39:17 +0000

    Update plotting.R

--- functions/plotting.R
+++ functions/plotting.R
@@ -4,8 +4,9 @@ plot_body_violin <- function(data) {
   data %>% 
     ggplot(aes(x = species, y = body_mass_g, colour = species, fill = species)) +
     geom_violin(alpha = 0.3, show.legend = FALSE) +
+    geom_boxplot(alpha = 0, show.legend = FALSE, width = 0.1) +
     theme_light() +
     # Renames the labels of the figure for clarity
-    labs(x = "Species", y = "Body mass (g)")+
+    labs(x = "Species", y = "Body mass (g)") +
     scale_x_discrete(labels=c('Adelie', 'Chinstrap', 'Gentoo'))
 }
